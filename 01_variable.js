624b8fe701d8298751e8ff6a6291c91d974c6b9d
bhongaderiya01 <bhongaderiya01@gmail.com>
2023-09-24 19:02:59 +0530

    design for understanding variable
    design for understanding variable

---
+++ 01_variable.js
@@ -0,0 +1,25 @@
+
+
+var firstName; //variable declaration
+firstName ="Riya" ; //variable initialization
+console.log( firstName);
+firstName = "Miss Riya" ;
+console.log(firstName);
+
+
+
+
+var lastName="Bhongade";//variable declaration and initialization
+console.log(lastName);
+
+var height ; //Variable Declaration
+console.log(height);
+height ="6";
+console.log(height);
+height ="6.2 feet";
+console.log(height);
+
+
+
+var marks="78%" ;
+console.log("10th marks:", marks ,"Grade A");
