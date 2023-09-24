2e837a26ba51fa85ecd629e3ed38e464d9d1919c
bhongaderiya01 <bhongaderiya01@gmail.com>
2023-09-24 19:01:55 +0530

    Swapping numbers in JS
    Swapping numbers in JS

---
+++ 01_swap.js
@@ -0,0 +1,14 @@
+
+
+
+
+//WAP to swap variable values using third variable
+var num1="100";
+var num2="200";
+console.log("============== Before Swap============");
+console.log(num1, num2);
+var temp= num1;
+num1 =num2;
+num2 = temp ;
+console.log("============== After Swap============");
+console.log(num1, num2);
