658ce9cc4faf1145952260f3d4c9aab8d73ced39
bhongaderiya01 <bhongaderiya01@gmail.com>
2023-09-24 23:26:30 +0530

    Program for typeof operator(assign2)
    Program for typeof operator(assign2)

---
+++ 02_typeOf.js
@@ -0,0 +1,36 @@
+
+
+
+console.log("========== Assignment 02 ==============");
+
+console.log("============ First Step ============");
+console.log("----- Before Initialization -------");
+
+var bankName ;
+console.log( bankName , "  Data type :" , typeof bankName );
+console.log("----- After Initialization -------");
+bankName = 'SBI Bank';
+console.log( bankName , "  Data type :" , typeof bankName );
+
+
+console.log("============ Second Step ============");
+var marks ='90%' ;
+console.log("Marks :" , marks , " Data type :" , typeof marks);
+
+
+console.log("============ Third Step ============");
+console.log("----- Before Initialization -------");
+var isWorking ;
+console.log("isWorking :", isWorking ," Data type:" ,typeof isWorking);
+isWorking = true ;
+console.log("isWorking :", isWorking ," Data type:" ,typeof isWorking);
+
+
+
+console.log("============ Fourth Step ============");
+console.log("----- Before Update -------");
+var totalCount ="Hundred and Seven " ;
+console.log( "totalCount :" ,totalCount, "Data type :" ,typeof totalCount );
+console.log("----- After Update -------");
+totalCount= 107 ;
+console.log( "totalCount :" ,totalCount, "  Data type :" ,typeof totalCount );
