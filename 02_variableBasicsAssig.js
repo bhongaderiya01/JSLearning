6474253937a2b353cedefd5f6390268f9d10fd2e
bhongaderiya01 <bhongaderiya01@gmail.com>
2023-09-24 22:52:37 +0530

    Program for data type(assign2)
    Program for data type(assign2)

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,37 @@
+
+
+
+console.log("============ Assignment 1 ============= ");
+console.log("======= Data Type : number ==========");
+ var pin = 444105;
+ console.log("Pin :", pin ,"type :", typeof pin);
+ var mobile = 444105;
+ console.log("Mobile :", mobile ,"type :", typeof mobile);
+ var accountNumber = 4575744105;
+ console.log("Account Number :",accountNumber ,"type :", typeof accountNumber);
+
+ console.log("========== Data Type : number ==========");
+var firstName="Riya" ;
+console.log("First Name :", firstName , "type :", typeof firstName);
+var lastName="Bhongade" ;
+console.log("Last Name :", lastName , "type :", typeof lastName);
+var city="Pune";
+console.log("City: ",city,"type :",typeof city );
+
+
+console.log("=========== Data Type : number ==========");
+var isIndian =true;
+console.log("IS Indian:", isIndian,"type:", typeof isIndian);
+var isChef= true ;
+console.log("Is Chef: ",isChef,"type:", typeof isChef);
+   
+
+console.log("========= Null vs Undefined ==========");
+console.log("Null : It is no value but not equal to 0");
+console.log("Undefined :When variable is declared and not assigned any value, then it is called Undefined ");
+
+
+
+
+
+
