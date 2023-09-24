d9a5d0bfe01f0bccc37669cc64d9c5758e48b901
bhongaderiya01 <bhongaderiya01@gmail.com>
2023-09-24 19:04:51 +0530

    Program for variable(assign_01)
    Program for variable(assign_01)

---
+++ 01_variableAssign.js
@@ -0,0 +1,25 @@
+
+
+var myFullName  ;
+console.log(myFullName);
+myFullName ="Riya Bhongade" ;
+console.log(myFullName);
+myFullName="Riya Suhas Bhongade";
+console.log(myFullName);
+var myLove ="I love only JS" ;
+console.log(myLove);
+
+var firstName="Riya" ;
+console.log("first name is:",firstName);
+var lastName="Bhongade";
+console.log("last name is:",lastName);
+var age="24";
+console.log("age is:", age);
+var address= "Amravati" ;
+console.log("Address:" , address);
+var pinCode="444601";
+console.log("Pincode:", pinCode);
+var gender ="female" ;
+console.log("Gender:" , gender);
+var isMarried ="no";
+console.log("Is Married :" , isMarried);
