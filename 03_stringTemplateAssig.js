d763dd002aa81edc27234e389b238e9b60a029ae
bhongaderiya01 <bhongaderiya01@gmail.com>
2023-10-06 09:19:49 +0530

    String Templete
    String Templete  (assign03)

---
+++ 03_stringTemplateAssig.js
@@ -0,0 +1,16 @@
+
+
+
+
+function stringBasics(hobby1, hobby2,hobby3){
+    var company = "cognizant";
+console.log(`1. My Dream Company is :${company}`);
+var Hobby1 = hobby1
+var Hobby2 = hobby2
+var Hobby3 = hobby3
+console.log(`2.1 My Hobbies are:${hobby1}, ${hobby2},${hobby3} `);
+console.log(`2.2. My Hobbies after concatination "${(Hobby1.concat(Hobby2)).concat(Hobby3)}"`);
+
+}
+stringBasics("listening music" ,"painting","traveling");
+
