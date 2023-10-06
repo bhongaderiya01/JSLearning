40938e8e91ec2c7b801b7565655f80f6205f0b54
bhongaderiya01 <bhongaderiya01@gmail.com>
2023-10-06 09:14:55 +0530

    string basic (function with parameter)
    string basic (function with parameter)

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,17 @@
+
+function stringBasics(hobby1, hobby2,hobby3){
+    console.log("======= Assignment 02=========");
+    var company = "cognizant";
+console.log("My Dream Company is :", company);
+console.log('--------------------');
+console.log('My Hobbies are:', hobby1,",",hobby2,",",hobby3);
+console.log('--------------------');
+console.log("Total character of hobby1:",hobby1.length);
+console.log("Total character of hobby2:",hobby2.length);
+console.log("Total character of hobby3:",hobby3.length);
+var result= hobby1.length + hobby2.length + hobby3.length
+console.log("Sum of three hobbies characters :",result);
+
+}
+stringBasics("listening music" ,"painting","traveling");
+
